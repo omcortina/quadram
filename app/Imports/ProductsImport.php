ed273ffdd8b9efb654195cdb58de3f59e3cc1847
Oscar Cortina <oscarcortina12@gmail.com>
2021-06-05 06:07:57 -0500

    arreglo importar

--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -16,18 +16,18 @@ class ProductsImport implements ToModel, WithHeadingRow
             $product->save();
         }
         return new Producto([
-            'estado' => $row['activo'] == true ? 1 : 0,
+            'estado' => $row['activoa'] == true ? 1 : 0,
             'codigo' => $row["referencia_interna"],
-            'codigo_barras' => $row["codigo_barras"],
+            'codigo_barras' => $row["codigo_de_barras"],
             'cantidad_mano' => $row["cantidad_a_mano"],
             'nombre' => $row["nombre"],
             'descripcion' => $row["descripcion"] ?? "",
-            'precio_venta' => $row["precio_venta"],
+            'precio_venta' => $row["precio_de_venta"],
             'unidad_medida' => $row["unidad_de_medida"],
             'codigo_invima' => $row["codigo_invima"],
             'fecha_vencimiento_invima' => date('Y-m-d', $row["venc_invima"]),
             'codigo_atc' => $row["codigo_atc"],
-            'codigo_ucm' => $row["codigo_ucm"],
+            'codigo_cum' => $row["codigo_cum"],
             'presentacion' => $row["presentacion"],
             'marca' => $row["marcalaboratorio"],
         ]);
